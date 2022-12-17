b260042c68a4f7821406867a43913ee4dd937866
Magical_Poof <50812178+TheMagicalPoof@users.noreply.github.com>
2022-12-17 16:35:50 +0300

    ds
    dx

--- backup.py
+++ backup.py
@@ -1,16 +1,99 @@
+import sys
+import shutil
 from paramiko import SSHClient, AutoAddPolicy
 import stat
 import os
+import datetime
 from sys import argv
+from configparser import ConfigParser
 
-ssh = SSHClient()
-ssh.set_missing_host_key_policy(AutoAddPolicy())
-ssh.connect(hostname=argv[1], username=argv[2], password=argv[3], port=int(argv[4]))
-sftp = ssh.open_sftp()
 
-with open("skip.txt", "r" if os.path.exists("skip.txt") else "w+") as file:
-    SKIP_LIST = file.read().split("\n")
-    file.close()
+# os.path.exists("")
+# ssh = SSHClient()
+# ssh.set_missing_host_key_policy(AutoAddPolicy())
+# ssh.connect(hostname=argv[1], username=argv[2], password=argv[3], port=int(argv[4]))
+# sftp = ssh.open_sftp()
+#
+# with open("skip.txt", "r" if os.path.exists("skip.txt") else "w+") as file:
+#     SKIP_LIST = file.read().split("\n")
+#     file.close()
+def main(config: ConfigParser):
+    create_path(config.get("backup", "path"))
+    print(get_backup_dir(config.get("backup", "path"), "test_1"))
+
+
+def get_config(name: str = "config.ini"):
+    cfg = ConfigParser()
+    if not os.path.exists(name):
+        cfg.add_section("backup")
+        cfg.set("backup", "path", "backup")
+        cfg.write(open(name, "w"))
+        sys.exit(f'{name} file has been created. Check your settings.')
+    cfg.read(name)
+    return cfg
+
+
+def get_credentials():
+    try:
+        cred_file = open("credentials.txt")
+    except FileNotFoundError:
+        sys.exit('Make a "credentials.txt" file, by "ip:port:login:password" string template.')
+    with cred_file:
+        lines = cred_file.read().splitlines()
+        credentials = {}
+
+        for enum, line in enumerate(lines):
+            try:
+                ip, port, login, password = line.split(":")
+            except ValueError:
+                sys.exit('Not correct data in "credentials.txt" file.')
+            credentials.update({enum: {"ip": ip,
+                                       "port": port,
+                                       "login": login,
+                                       "password": password}})
+
+        if not bool(credentials):
+            sys.exit('Not correct data in "credentials.txt" file.')
+
+        return credentials
+
+
+def create_path(path: str):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def get_backup_dir(bpath: str, fname: str, bcount: int = 5, time_format: str = "%Y-%m-%d %H.%M.%S"):
+    date_now = str(datetime.datetime.now().strftime(time_format))
+    if os.path.exists(f"{bpath}/{fname}"):
+        dir_list = os.listdir(f"{bpath}/{fname}")
+        # if len(dir_list) > 0
+        print("gg")
+        shutil.copytree(f"{bpath}/{fname}/{max(dir_list)}",
+                        f"{bpath}/{fname}/{date_now}")
+        if len(dir_list) >= bcount:
+            shutil.rmtree(f"{bpath}/{min(dir_list)}")
+        return f"{bpath}/{date_now}"
+
+    os.mkdir(f"{bpath}/{fname}/{date_now}")
+    return f"{bpath}/{fname}/{date_now}"
+
+
+def copy_prev_backup(path: str, dir_list: list, timestamp: str):
+    if bool(dir_list):
+        shutil.copytree(f"{path}/{max(dir_list)}",
+                        f"{path}/{timestamp}")
+
+
+def rm_oldest(path: str, dir_list: list, count: int = 5):
+    dir_list_len = len(dir_list)
+    if dir_list_len <= 1:
+        return
+
+    for _ in range(count - dir_list_len):
+        shutil.rmtree(f"{path}/{min(dir_list)}")
+
+# print(get_credentials())
 
 
 def scan_dir(sftp_path: str, local_path: str):
@@ -38,6 +121,7 @@ def scan_dir(sftp_path: str, local_path: str):
 
         sftp.get(ex_file_path, local_file_path)
 
+
 def download(sftp_path: str, local_path: str):
     try:
         os.mkdir(local_path)
@@ -48,7 +132,9 @@ def download(sftp_path: str, local_path: str):
     scan_dir(sftp_path, local_path)
 
 
-download("mc_paper", "example")
-# sftp.get("mc_paper/bukkit.yml", "example/mc_paper/bukkit.yml")
-ssh.close()
-print("fin")
+if __name__=="__main__":
+    main(get_config())
+
+# download("mc_paper", "example")
+# # sftp.get("mc_paper/bukkit.yml", "example/mc_paper/bukkit.yml")
+# ssh.close()
